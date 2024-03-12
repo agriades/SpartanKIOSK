719da9e03ca9843961aa70e1cb009b6cab39ef44
Sooyoung Hwang <agriades23@gmail.com>
2024-03-12 12:42:23 +0900

    main method is play-able; korean not available now on bluetooth keyboard

---
+++ myKIOSK/app/src/main/java/com/example/mykiosk/kioskLv1.kt
@@ -0,0 +1,13 @@
+package com.example.mykiosk
+fun main(){
+    println("===SpartanSUBWAY lv 1.==="); println("")
+    println("안녕하세요! 서브웨이 내배캠 지점입니다. 원하는 메뉴를 선택하세요!")
+    println("")
+    println("---[서브웨이 메뉴]---")
+    println("1. 단품 샌드위치")
+    println("2. 음료")
+    println("3. 사이드")
+    println("0. 종료")
+}
+
+

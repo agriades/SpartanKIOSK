f2d61466a2d79e4ae39f0c633106abe0f5cf82df
Sooyoung Hwang <agriades23@gmail.com>
2024-03-13 17:07:58 +0900

    클래스 시도 1회차

--- myKIOSK/app/src/main/java/com/example/mykiosk/kioskLv2.kt
+++ myKIOSK/app/src/main/java/com/example/mykiosk/kioskLv2.kt
@@ -1,87 +1,129 @@
 package com.example.mykiosk
-//lv2 작성 중
-fun intro(){
-    println("===SpartanSUBWAY lv 1.==="); println("")
+import kotlin.arrayOf
+
+//lv 2
+class Sandwich(var name: String = "베지", var price: Int = 4900){
+    fun displayInfo(): Unit {
+        println("${name}: ${price}")
+    }
+    companion object {
+        fun desc() {
+            println("")
+            println("---[단품 샌드위치]---")
+            println("1. 스파이시쉬림프 7,900원")
+            println("2. 에그마요 5,500원")
+            println("3. 베지 4,900원")
+            println("0. 처음으로")
+        }
+    }
+}
+class Drink(var name: String = "탄산음료",var price: Int = 2000) {
+    fun displayInfo(): Unit {
+        println("${name}: ${price}")
+    }
+
+    companion object {
+        fun desc() {
+            println("")
+            println("---[음료]---")
+            println("1. 탄산음료 2,000원")
+            println("2. 물 1,000원")
+            println("0. 처음으로")
+        }
+    }
+}
+class Side(var name: String = "쿠키",var price: Int = 1300) {
+    fun displayInfo(): Unit {
+        println("${name}: ${price}")
+    }
+
+    companion object {
+        fun desc() {
+            println("")
+            println("---[사이드]---")
+            println("1. 쿠키 1,300원")
+            println("2. 감자칩 1,300원")
+            println("0. 처음으로")
+        }
+    }
+}
+
+fun descIntro(){
+    println("===SpartanSUBWAY lv 2.==="); println("")
     println("안녕하세요! 서브웨이 내배캠 지점입니다. 원하는 메뉴를 선택하세요!")
     println("")
-}
-fun menu(): String {
     println("---[서브웨이 메뉴]---")
     println("1. 단품 샌드위치")
     println("2. 음료")
     println("3. 사이드")
     println("0. 종료")
-    return readln()
 }
-fun sandwich(): Int{
-    val sandwich1 = Sandwich()
-    val sandwich2 = Sandwich("에그마요", 7900)
-    val sandwich3 = Sandwich("스파이시 쉬림프", 7900)
-    println("---[단품 샌드위치]---")
-    sandwich1.displayInfo("1")
-    sandwich2.displayInfo("2")
-    sandwich3.displayInfo("3")
-    var price: Int = 0
-    when(readln()){
-        "1" -> price = sandwich1.price
-        "2" -> price = sandwich2.price
-        "3" -> price = sandwich3.price
-    }
-    return price
+fun descMore(){
+    println("더 주문하고 싶으신가요?")
+    println("---[서브웨이 메뉴]---")
+    println("1. 단품 샌드위치")
+    println("2. 음료")
+    println("3. 사이드")
+    println("0. 종료")
+    println("-1. 결제하기")
 }
-fun drink(): Int{
-    val drink1 = Drink()
-    val drink2 = Drink("물", 1000)
-    println("---[음료]---")
-    drink1.displayInfo("1")
-    drink2.displayInfo("2")
-    var price: Int = 0
-    when (readln()){
-        "1" -> price = drink1.price
-        "2" -> price = drink2.price
-    }
-    return price
-}
-fun side(): Int{
-    val side1 = Side()
-    val side2 = Side("감자칩", 1300)
-    println("---[사이드]---")
-    side1.displayInfo("1")
-    side2.displayInfo("2")
-    var price: Int = 0
-    when (readln()){
-        "1" -> price = side1.price
-        "2" -> price = side2.price
-    }
-    return price
-}
-
-class Sandwich constructor(var name: String = "베지", var price: Int = 4900){
-    fun displayInfo(index: String): Unit {
-        println("${index}. ${name}: ${price}")
-    }
-}
-class Drink constructor(var name: String = "탄산음료", var price: Int = 2000){
-    fun displayInfo(index: String): Unit {
-        println("${index}. ${name}: ${price}")
-    }
-}
-class Side constructor(var name: String = "쿠키", var price: Int = 1300){
-    fun displayInfo(index: String): Unit {
-        println("${index}. ${name}: ${price}")
-    }
+fun descEnd(price: Int){
+    println(""); println("${price}원을 결제합니다.")
+    println("결제가 완료되었습니다! 이용해주셔서 감사합니다."); println("")
+    println("===SpartanSUBWAY lv 2.===")
 }
 fun main(){
+    val sandwich = Sandwich()
+    val drink = Drink()
+    val side = Side()
+
+    var receipt: Array<String> = arrayOf()
+    var isBuying = true
+    descIntro()
     var price: Int = 0
-    var isKioskOn = true
-    intro()
-    while(isKioskOn){
-        when(menu()) {
-            "1" -> price += sandwich()
-            "2" -> price += drink()
-            "3" -> price += side()
-            else -> break
+    while(isBuying){
+        val getIntro = readln()
+        if (getIntro == "0") break
+        else if (getIntro == "-1") {isBuying = false; break}
+        else if (getIntro == "1"){
+            Sandwich.desc()
+            when(readln()){
+                "1" -> {println("스파이시 쉬림프 1개를 주문 목록에 추가했습니다."); price += 7900
+                receipt += "스파이시 쉬림프 1 - 7,900원"}
+                "2" -> {println("에그마요 1개를 주문 목록에 추가했습니다"); price += 5500
+                receipt += "에그마요 1 - 5,500원"}
+                "3" -> {println("베지 1개를 주문 목록에 추가했습니다"); price += 4900
+                receipt += "베지 1 - 4,900원" }
+                "0" -> continue
+
+            }
         }
+        else if(getIntro == "2"){
+            Drink.desc()
+            when(readln()){
+                "1" -> {println("탄산음료 1잔을 주문 목록에 추가했습니다."); price += 2000
+                    receipt += "탄산음료 1 - 2,000원"}
+                "2" -> {println("생수 1병을 주문 목록에 추가했습니다."); price += 1000
+                    receipt += "생수 1 - 1,000원"}
+                "0" -> continue
+            }
+        }
+        else if (getIntro == "3"){
+            Side.desc()
+            when(readln()){
+                "1" -> {println("쿠키 1개를 주문 목록에 추가했습니다."); price += 1300
+                    receipt += "쿠키 1 - 1,300원"}
+                "2" -> {println("감자칩 1개를 주문 목록에 추가했습니다."); price += 1300
+                    receipt += "감자칩 1 - 1,300원"}
+                "0" -> continue
+            }
+        }
+        println("결제 금액은 ${price}원 입니다.")
+        println("")
+        descMore()
     }
-    println("총 결제 가격은 ${price}입니다.")
+
+
+    receipt.forEach { println(it) }
+    descEnd(price)
 }
